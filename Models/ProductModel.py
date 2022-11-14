76f34ff14ec15decbff0c1dfa434314bda64ccfa
Morteza Karimi <mortezak1373@gmail.com>
2022-11-14 10:25:10 +0330

    fix best deal and style table

--- Models/ProductModel.py
+++ Models/ProductModel.py
@@ -1,5 +1,8 @@
+import typing
+
 from PyQt6 import QtCore
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QBrush, QColorConstants
 from PyQt6.QtSql import QSqlQuery, QSqlQueryModel
 
 
@@ -15,13 +18,18 @@ class ProductModel(QSqlQueryModel):
        product."amazon_rating_stars",
        product."amazon_rating_total",
        product."fake_spot_grade",
-       product."fake_spot_rating"
+       product."fake_spot_rating",
+       CASE
+           WHEN product.highest_price + product.lowest_price == 0 THEN null
+           ELSE (((product.highest_price + product.lowest_price) / 2) - product.price) /
+                      (product.highest_price + product.lowest_price) * 100 END as deal
 FROM product
      LEFT JOIN search_term_product stp on product.id = stp.product_id
      LEFT JOIN search_term st on stp.search_keyword = st.keyword'''
 
     def __init__(self, *args, **kwargs):
         super(ProductModel, self).__init__(*args, **kwargs)
+        self.colorize = False
         self.default_query = QSqlQuery()
         self.default_query.prepare(self.DEFAULT_QUERY)
         self.default_query.exec()
@@ -37,11 +45,13 @@ FROM product
         self.setHeaderData(8, Qt.Orientation.Horizontal, "Amazon Rating Total")
         self.setHeaderData(9, Qt.Orientation.Horizontal, "Fake Spot Grade")
         self.setHeaderData(10, Qt.Orientation.Horizontal, "Fake Spot Rating")
+        self.setHeaderData(11, Qt.Orientation.Horizontal, "Deal")
 
     def flags(self, index: QtCore.QModelIndex):
         return Qt.ItemFlag.ItemIsEnabled
 
     def asLowestPrice(self):
+        self.colorize = True
         self.default_query.prepare('''
         SELECT st.keyword,
        product."id",
@@ -53,7 +63,11 @@ FROM product
        product."amazon_rating_stars",
        product."amazon_rating_total",
        product."fake_spot_grade",
-       product."fake_spot_rating"
+       product."fake_spot_rating",
+       CASE
+           WHEN product.highest_price + product.lowest_price == 0 THEN null
+           ELSE (((product.highest_price + product.lowest_price) / 2) - product.price) /
+                      (product.highest_price + product.lowest_price) * 100 END as deal
 FROM product
      LEFT JOIN search_term_product stp on product.id = stp.product_id
      LEFT JOIN search_term st on stp.search_keyword = st.keyword
@@ -62,6 +76,7 @@ FROM product
         self.setQuery(self.default_query)
 
     def asHighestRating(self):
+        self.colorize = True
         self.default_query.prepare('''
                 SELECT st.keyword,
                product."id",
@@ -73,7 +88,11 @@ FROM product
                product."amazon_rating_stars",
                product."amazon_rating_total",
                product."fake_spot_grade",
-               product."fake_spot_rating"
+               product."fake_spot_rating",
+               CASE
+           WHEN product.highest_price + product.lowest_price == 0 THEN null
+           ELSE (((product.highest_price + product.lowest_price) / 2) - product.price) /
+                      (product.highest_price + product.lowest_price) * 100 END as deal
         FROM product
              LEFT JOIN search_term_product stp on product.id = stp.product_id
              LEFT JOIN search_term st on stp.search_keyword = st.keyword
@@ -83,22 +102,54 @@ FROM product
         self.setQuery(self.default_query)
 
     def asBestDeal(self):
+        self.colorize = True
         self.default_query.prepare('''
                 SELECT st.keyword,
-                   product."id",
-                   product."title",
-                   product."price",
-                   product."highest_price",
-                   product."lowest_price",
-                   product."link",
-                   product."amazon_rating_stars",
-                   product."amazon_rating_total",
-                   product."fake_spot_grade",
-                   product."fake_spot_rating"
-        FROM product
-             LEFT JOIN search_term_product stp on product.id = stp.product_id
-             LEFT JOIN search_term st on stp.search_keyword = st.keyword
-             WHERE product.price <> 0 ORDER BY (((product.highest_price + product.lowest_price) / 2) - product.price)  DESC''')
+       product."id",
+       product."title",
+       product."price",
+       product."highest_price",
+       product."lowest_price",
+       product."link",
+       product."amazon_rating_stars",
+       product."amazon_rating_total",
+       product."fake_spot_grade",
+       product."fake_spot_rating",
+       CASE
+           WHEN product.highest_price + product.lowest_price == 0 THEN null
+           ELSE (((product.highest_price + product.lowest_price) / 2) - product.price) /
+                      (product.highest_price + product.lowest_price) * 100 END as deal
+FROM product
+         LEFT JOIN search_term_product stp on product.id = stp.product_id
+         LEFT JOIN search_term st on stp.search_keyword = st.keyword
+WHERE product.price <> 0
+ORDER BY deal DESC''')
         self.default_query.exec()
         self.removeColumn(11)
         self.setQuery(self.default_query)
+
+    def data(self, item: QtCore.QModelIndex, role: int = ...) -> typing.Any:
+        if not item.isValid():
+            return QSqlQueryModel.data(self, item, role)
+            # vvvv this is the magic part
+        elif role == Qt.ItemDataRole.BackgroundRole and self.colorize:
+            match item.row():
+                case 0:
+                    return QBrush(QColorConstants.Svg.green)
+
+                case 1:
+                    return QBrush(QColorConstants.Svg.forestgreen)
+
+                case 2:
+                    return QBrush(QColorConstants.Svg.limegreen)
+
+        if role == Qt.ItemDataRole.DisplayRole and item.column() == 11:
+            deal = QSqlQueryModel.data(self, item, role)
+            if deal != "":
+                deal = round(deal,2)
+                print(deal)
+                return f"{deal}%"
+            return deal
+
+        # ^^^^ this is the magic part
+        return QSqlQueryModel.data(self, item, role)
